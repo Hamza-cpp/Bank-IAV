53f0d515f1e5d074c0bb1a27bb25fbd8773b28d5
Hamza-cpp <hamza.okhadir2018@gmail.com>
2024-02-27 18:54:29 +0100

    small changes in the models classes

--- app/models/role.py
+++ app/models/role.py
@@ -1,11 +1,16 @@
 from app.models import db
 from app.models.association import User_Role
 
+ADMIN = "admin"
+CLIENT = "client"
+
 
 class Role(db.Model):
     __tablename__ = "roles"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True, nullable=False)# eg. "admin", "client"
+    name = db.Column(
+        db.String, unique=True, nullable=False, default="client"
+    )  # eg. "admin", "client"
     description = db.Column(db.String)
     # Relation many to many Role <--> User
     users = db.relationship("User", secondary=User_Role, back_populates="roles")
--- app/models/user_profile.py
+++ app/models/user_profile.py
@@ -9,6 +9,7 @@ class UserProfile(db.Model):
     first_name = db.Column(db.String(50))
     last_name = db.Column(db.String(50))
     phone_number = db.Column(db.String(20), nullable=True)
+    gender = db.Column(db.String(20), nullable=True)
     address = db.Column(db.String(200), nullable=True)
     #Relation to user table
     user = db.relationship("User", back_populates="profile")
